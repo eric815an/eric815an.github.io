02cff6d76ebd1ce416f6beb0468824a5ee3de141
shnay0815 <35589670+shnay0815@users.noreply.github.com>
2018-10-18 00:48:55 -0500

    DONE

--- js/index.js
+++ js/index.js
@@ -15,9 +15,6 @@ const send = document.getElementById("btn");
 const list = document.getElementById("list");
 const todos = firebase.database().ref("todos");
 
-/*
-** 添加 todos
-*/
 send.addEventListener("click", () => {
 	todos.push({
 		content: txt.value
@@ -26,26 +23,14 @@ send.addEventListener("click", () => {
 });
 
 
-
-
-
-/*
-** 雙向綁定，動態更新
-*/
 todos.on("value", snapshot => {
 	let str = "";
-	// let data = snapshot.val();
 	let data = [];
 	snapshot.forEach(item => {
 		data.push({ key: item.key, content: item.val().content });
 	});
 
-	// 讓新加入的 todo 排到最上面
 	data.reverse();
-
-	// for (let item in data) {
-	// 	str += `<li data-key="${item}">${data[item].content}</li>`;
-	// }
 
 	for (let i = 0; i < data.length; i++) {
 		str += `<li data-key="${data[i].key}">${data[i].content}</li>`;
@@ -54,12 +39,6 @@ todos.on("value", snapshot => {
 });
 
 
-
-
-
-/*
-** 移除 todos
-*/
 list.addEventListener("click", evt => {
 	if (evt.target.nodeName === "LI") {
 		let key = evt.target.dataset.key;
