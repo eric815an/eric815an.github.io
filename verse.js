d7769c5109c8aa32551458c5b219227ff1b6e99d
shnay0815 <35589670+shnay0815@users.noreply.github.com>
2018-10-17 21:02:35 -0500

    First commit

---
+++ verse.js
@@ -0,0 +1,68 @@
+console.clear();
+
+let config = {
+	apiKey: "AIzaSyCzBZ78VCgB8aMM6qsEMG6LXxYFC_pOVzo",
+	authDomain: "verse-a1e96.firebaseapp.com",
+	databaseURL: "https://verse-a1e96.firebaseio.com",
+	projectId: "verse-a1e96",
+	storageBucket: "verse-a1e96.appspot.com",
+	messagingSenderId: "490796473738"
+};
+firebase.initializeApp(config);
+
+const txt = document.getElementById("txt");
+const send = document.getElementById("btn");
+const list = document.getElementById("list");
+const todos = firebase.database().ref("todos");
+
+/*
+** 添加 todos
+*/
+send.addEventListener("click", () => {
+	todos.push({
+		content: txt.value
+	});
+	txt.value = "";
+});
+
+
+
+
+
+/*
+** 雙向綁定，動態更新
+*/
+todos.on("value", snapshot => {
+	let str = "";
+	// let data = snapshot.val();
+	let data = [];
+	snapshot.forEach(item => {
+		data.push({ key: item.key, content: item.val().content });
+	});
+
+	// 讓新加入的 todo 排到最上面
+	data.reverse();
+
+	// for (let item in data) {
+	// 	str += `<li data-key="${item}">${data[item].content}</li>`;
+	// }
+
+	for (let i = 0; i < data.length; i++) {
+		str += `<li data-key="${data[i].key}">${data[i].content}</li>`;
+	}
+	list.innerHTML = str;
+});
+
+
+
+
+
+/*
+** 移除 todos
+*/
+list.addEventListener("click", evt => {
+	if (evt.target.nodeName === "LI") {
+		let key = evt.target.dataset.key;
+		todos.child(key).remove();
+	}
+});
